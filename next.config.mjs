3de4d3d14c9d1ee2731b8f797f2fe97cebd11954
Smart <jkscur@gmail.com>
2026-04-01 09:20:18 -0700

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+  basePath: "/admin",
   outputFileTracingRoot: path.join(__dirname, ".."),
   images: {
     remotePatterns: [
